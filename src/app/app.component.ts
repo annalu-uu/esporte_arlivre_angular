54a0c0b69cfd8174e0614cf1ec7c1b1dbdb6e36d
Anna Luiza Santos Costa <anna.l.costa6@aluno.senai.br>
2026-08-13 16:44:36 -0300

    alterada as pastas e criadas as rotas no routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 import { MenuComponentComponent } from './component/menu-component/menu-component.component';
 
-import { AtletaComponent } from './component/atleta/atleta.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponentComponent, AtletaComponent],
+  imports: [RouterOutlet, MenuComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

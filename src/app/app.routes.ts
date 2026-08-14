f5f5cb6ed6ce7542b18e9456083b8816129c6385
Anna Luiza Santos Costa <anna.l.costa6@aluno.senai.br>
2026-08-14 14:01:29 -0300

    alterada e criada a pasta models

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-
 import { HomeComponent } from './component/home/home.component';
 import { AtletaComponent } from './component/atleta/atleta.component';
 

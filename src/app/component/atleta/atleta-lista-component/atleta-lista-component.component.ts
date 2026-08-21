241c24852e893976987c71a2209bf49d240a6cbb
Anna Luiza Santos Costa <anna.l.costa6@aluno.senai.br>
2026-08-21 16:47:25 -0300

    alterado

--- src/app/component/atleta/atleta-lista-component/atleta-lista-component.component.ts
+++ src/app/component/atleta/atleta-lista-component/atleta-lista-component.component.ts
@@ -29,7 +29,7 @@ export class AtletaListaComponent {
       .subscribe({
         next: (dadosAtletas) => {
          
-          this.listaAtletas.set([...dadosAtletas].sort((a, b) => a.nome.localeCompare(b.nome)))
+          this.listaAtletas.set([...dadosAtletas].sort((a, b) => (a.nome || '').localeCompare(b.nome || '')))
 
           console.table(this.listaAtletas())
         },
